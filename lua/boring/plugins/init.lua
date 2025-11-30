8d4a6d96b18a3f3dea139aac4177112c32c56421
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-11-30 05:38:16 +0100

    feat: add oil

--- lua/boring/plugins/init.lua
+++ lua/boring/plugins/init.lua
@@ -2,4 +2,5 @@ require('lze').load({
     { import = 'boring.plugins.mini' },
     { import = 'boring.plugins.treesitter' },
     { import = 'boring.plugins.lsp' },
+    { import = 'boring.plugins.oil' },
 })

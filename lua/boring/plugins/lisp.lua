dab6a93313cbfbd70fa77540427f980716347478
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-11-30 05:38:16 +0100

    fix(lisp): fix category

--- lua/boring/plugins/lisp.lua
+++ lua/boring/plugins/lisp.lua
@@ -1,14 +1,14 @@
 return {
     {
         'parinfer-rust',
-        for_cat = 'off',
+        for_cat = 'lisp',
         dep_of = { 'conjure' },
     },
     {
         'conjure',
         ft = { 'lisp', 'clojure', 'fennel', 'hy', 'scheme', 'yuck' },
         on_require = 'conjure',
-        for_cat = 'off',
+        for_cat = 'lisp',
         after = function(_)
             vim.g['conjure#debug'] = true
         end,

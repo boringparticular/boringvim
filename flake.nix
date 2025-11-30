5639ed8208e33dce00016793c85c4e906b23b0ed
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-11-30 05:38:16 +0100

    feat: add flake support

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixCats.url = "github:BirdeeHub/nixCats-nvim";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixCats,
+      ...
+    }@inputs:
+    {
+      packages = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ ];
+            config = { };
+          };
+        in
+        nixCats.utils.mkAllWithDefault (import ./. (inputs // { inherit pkgs; }))
+      );
+      homeModule = self.packages.x86_64-linux.default.homeModule; # <- it will get the system from the importing configuration
+      nixosModule = self.packages.x86_64-linux.default.nixosModule; # <- module namespace defaults to defaultPackageName.{ enable, packageNames, etc... }
+    };
+}

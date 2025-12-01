a12455d9d8eaab6d0d8962c4e006ff036865aa1b
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: add go support

---
+++ lua/boring/plugins/go.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        'go-nvim',
+        for_cat = 'general',
+        event = { 'CmdlineEnter' },
+        ft = { 'go', 'gomod' },
+        after = function(_)
+            require('go').setup()
+        end,
+    },
+}

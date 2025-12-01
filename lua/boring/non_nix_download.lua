0461739b7b28dddbb90cb87ecee76930cf59fcc8
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: add some plugins to packer

--- lua/boring/non_nix_download.lua
+++ lua/boring/non_nix_download.lua
@@ -1,1 +1,14 @@
-require('nixCatsUtils.catPacker').setup({})
+-- TODO: Add missing plugins
+require('nixCatsUtils.catPacker').setup({
+    { 'BirdeeHub/lze' },
+    { 'catppuccin/nvim' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'echasnovski/mini.nvim' },
+
+    { 'stevearc/oil.nvim' },
+    { 'nvim-tree/nvim-web-devicons' },
+
+    { 'tpope/vim-sleuth', opt = true },
+    { 'folke/snacks.nvim', opt = true },
+    { 'obisdian-nvim/obsidian.nvim', opt = true },
+})

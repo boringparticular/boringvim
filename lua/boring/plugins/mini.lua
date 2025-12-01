56554c6832c4474484cb1febd281e32975812518
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat(statusline): display info from arrow plugin

--- lua/boring/plugins/mini.lua
+++ lua/boring/plugins/mini.lua
@@ -5,7 +5,38 @@ return {
         event = 'DeferredUIEnter',
         after = function(_)
             local statusline = require('mini.statusline')
-            statusline.setup({})
+            statusline.setup({
+                content = {
+                    active = function()
+                        local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+                        local git = MiniStatusline.section_git({ trunc_width = 40 })
+                        local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+                        local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+                        local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+                        local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+                        local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+                        local location = MiniStatusline.section_location({ trunc_width = 75 })
+                        local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+                        local arrow = pcall(require, 'arrow')
+                            and {
+                                hl = 'ArrowFileIndex',
+                                strings = { require('arrow.statusline').text_for_statusline_with_icons() },
+                            }
+
+                        return MiniStatusline.combine_groups({
+                            { hl = mode_hl, strings = { mode } },
+                            { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+                            arrow,
+                            '%<', -- Mark general truncate point
+                            { hl = 'MiniStatuslineFilename', strings = { filename } },
+                            '%=', -- End left alignment
+                            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                            { hl = mode_hl, strings = { search, location } },
+                        })
+                    end,
+                },
+            })
             ---@diagnostic disable-next-line: duplicate-set-field
             statusline.section_location = function()
                 return '%2l:%-2v'

0c72fc6626627a059292e373a4195a47f8ccda20
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: add configuration for diagnostics

--- lua/boring/init.lua
+++ lua/boring/init.lua
@@ -17,5 +17,40 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
+local icons = require('boring.icons')
+-- NOTE: can i disable virtual text for the current line and instead show the virtual line?
+vim.diagnostic.config({
+    underline = true,
+    virtual_text = {
+        current_line = false,
+        spacing = 4,
+        source = 'if_many',
+        prefix = function(diagnostic, _, _)
+            for d, icon in pairs(icons.diagnostics) do
+                if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+                    return icon
+                end
+            end
+            return '●'
+        end,
+    },
+    virtual_lines = {
+        current_line = true,
+    },
+    severity_sort = true,
+    update_in_insert = true,
+    float = {
+        border = 'rounded',
+    },
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = icons.diagnostics.error,
+            [vim.diagnostic.severity.WARN] = icons.diagnostics.warn,
+            [vim.diagnostic.severity.INFO] = icons.diagnostics.info,
+            [vim.diagnostic.severity.HINT] = icons.diagnostics.hint,
+        },
+    },
+})
+
 require('boring.colorscheme')
 require('boring.plugins')

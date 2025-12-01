57e7f2c930f5a65124088a7c03d1b730556e2d53
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat(grapple): add keymaps

--- lua/boring/plugins/init.lua
+++ lua/boring/plugins/init.lua
@@ -137,9 +137,18 @@ require('lze').load({
     {
         'grapple.nvim',
         for_cat = 'general.extra',
+        event = { 'BufReadPost', 'BufNewFile' },
+        cmd = 'Grapple',
+        keys = {
+            { '<leader>gm', '<cmd>Grapple toggle<cr>', desc = 'Grapple toggle tag' },
+            { '<leader>gM', '<cmd>Grapple toggle_tags<cr>', desc = 'Grapple open tags window' },
+            { '<leader>gn', '<cmd>Grapple cycle_tags next<cr>', desc = 'Grapple cycle next tag' },
+            { '<leader>gp', '<cmd>Grapple cycle_tags prev<cr>', desc = 'Grapple cycle previous tag' },
+        },
         after = function(_)
             require('grapple').setup({
                 icons = true,
+                scope = 'git',
             })
         end,
     },

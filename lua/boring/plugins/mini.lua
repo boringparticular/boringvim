4fbb443ba8de63f9288e47ac84fd021e155badfd
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat(mini): add more plugins

--- lua/boring/plugins/mini.lua
+++ lua/boring/plugins/mini.lua
@@ -1,5 +1,4 @@
 return {
-
     {
         'mini.nvim',
         for_cat = 'general',
@@ -15,6 +14,47 @@ return {
             require('mini.icons').setup()
             require('mini.surround').setup()
             require('mini.ai').setup({ n_lines = 500 })
+            require('mini.trailspace').setup()
+            -- NOTE: Do i need this in neovim >- 0.10?
+            require('mini.comment').setup()
+            require('mini.diff').setup()
+            require('mini.git').setup()
+            require('mini.splitjoin').setup()
+            require('mini.move').setup()
+            require('mini.sessions').setup({
+                autoread = true,
+            })
+            require('mini.operators').setup()
+            require('mini.bracketed').setup()
+            require('mini.files').setup()
+
+            -- NOTE: maybe replace this with catgoose/nvim-colorizer.lua
+            local hipatterns = require('mini.hipatterns')
+            hipatterns.setup({
+                highlighters = {
+                    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+                    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+                    hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+                    todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+                    note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+
+                    -- Highlight hex color strings (`#rrggbb`) using that color
+                    hex_color = hipatterns.gen_highlighter.hex_color(),
+                },
+            })
+
+            require('mini.animate').setup({
+                cursor = { enable = true },
+                resize = { enable = true },
+                scroll = { enable = false },
+                open = { enable = true },
+                close = { enable = true },
+            })
+
+            -- vim.keymap.set('n', '<C-u>', '<Cmd>lua vim.cmd("normal! <C-u>"); MiniAnimate.execute_after("scroll", "normal! zz")<CR>')
+            -- vim.keymap.set('n', '<C-d>', '<Cmd>lua vim.cmd("normal! <C-d>"); MiniAnimate.execute_after("scroll", "normal! zz")<CR>')
+            -- vim.keymap.set('n', 'n', '<Cmd>lua vim.cmd("normal! n"); MiniAnimate.execute_after("scroll", "normal! zzvz")<CR>')
+            -- vim.keymap.set('n', 'N', '<Cmd>lua vim.cmd("normal! N"); MiniAnimate.execute_after("scroll", "normal! zzvz")<CR>')
         end,
     },
 }

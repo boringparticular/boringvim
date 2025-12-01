78137c3f936d9a9e29e2ba33bee23efe3e75c1ad
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: add basic options

--- lua/boring/init.lua
+++ lua/boring/init.lua
@@ -3,6 +3,8 @@ vim.g.maplocalleader = ','
 
 require('lze').register_handlers(require('nixCatsUtils.lzUtils').for_cat)
 
+require('boring.options')
+
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),

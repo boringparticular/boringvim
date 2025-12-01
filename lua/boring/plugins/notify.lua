da347131012b91e11935ebbd797bdfbafc589c6e
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: disable notify

--- lua/boring/plugins/notify.lua
+++ lua/boring/plugins/notify.lua
@@ -1,12 +1,13 @@
 return {
     {
         'rcarriga/nvim-notify',
-        enabled = require('nixCatsUtils').enableForCategory('extra'),
+        enabled = false, -- require('nixCatsUtils').enableForCategory('extra'),
         opts = {
             max_width = 80,
             render = 'wrapped-compact',
         },
         config = function()
+            -- vim.notify = require('notify')
             vim.keymap.set('n', '<leader>sn', '<cmd>Telescope notify<CR>', { desc = '[S]earch [N]otifications' })
         end,
     },

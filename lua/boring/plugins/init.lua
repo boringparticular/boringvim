0b728f35100022daedc1fd7e43843d770b0ac7cd
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat(grug-far): add keymap

--- lua/boring/plugins/init.lua
+++ lua/boring/plugins/init.lua
@@ -112,6 +112,24 @@ require('lze').load({
     {
         'grug-far.nvim',
         for_cat = 'general.extra',
+        cmd = 'GrugFar',
+        keys = {
+            {
+                '<leader>sr',
+                function()
+                    local grug = require('grug-far')
+                    local ext = vim.bo.buftype == '' and vim.fn.expand('%:e')
+                    grug.open({
+                        transient = true,
+                        prefills = {
+                            filesFilter = ext and ext ~= '' and '*.' .. ext or nil,
+                        },
+                    })
+                end,
+                mode = { 'v', 'n' },
+                desc = '[S]earch and [R]eplace',
+            },
+        },
         after = function(_)
             require('grug-far').setup({})
         end,

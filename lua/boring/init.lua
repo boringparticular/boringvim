7b2bfe2c8c2e56a46b09d55f1a29963cdaea080b
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: register lze handle to support nix categories

--- lua/boring/init.lua
+++ lua/boring/init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
+require('lze').register_handlers(require('nixCatsUtils.lzUtils').for_cat)
+
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),

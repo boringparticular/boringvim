0af482d5b757ff58995c41f98aca0f3d8be5cbee
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat(blink): use mini icons

--- lua/boring/plugins/blink.lua
+++ lua/boring/plugins/blink.lua
@@ -92,6 +92,16 @@ return {
                         draw = {
                             treesitter = { 'lsp' },
                             components = {
+                                kind_icon = {
+                                    text = function(ctx)
+                                        local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                                        return kind_icon
+                                    end,
+                                    highlight = function(ctx)
+                                        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                                        return hl
+                                    end,
+                                },
                                 source_name = {
                                     text = function(ctx)
                                         return '[' .. ctx.source_name .. ']'

23e15e5968decadf04c22cac27e7a3ced8f40321
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: add initial config files

---
+++ init.lua
@@ -0,0 +1,7 @@
+require('nixCatsUtils').setup({
+    non_nix_value = true,
+})
+require('boring.non_nix_download')
+require('boring')
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/boring/non_nix_download.lua
@@ -0,0 +1,1 @@
+require('nixCatsUtils.catPacker').setup({})

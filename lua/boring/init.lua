025684688627f24d7f8ebaeb1f5a0cf1caae4e80
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat: move colorscheme in separate file

--- lua/boring/init.lua
+++ lua/boring/init.lua
@@ -14,63 +14,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
-require('catppuccin').setup({
-    flavour = 'mocha',
-    term_colors = true,
-    dim_inactive = {
-        enabled = true,
-        shade = 'dark',
-        percentage = 0.15,
-    },
-    custom_highlights = function(colors)
-        return {
-            BlinkCmpKind = { fg = colors.blue },
 
-            BlinkCmpMenu = { fg = colors.text, bg = colors.base },
-            -- BlinkCmpMenuBorder = { fg = colors.overlay0, bg = colors.base },
-            BlinkCmpMenuBorder = { fg = colors.blue, bg = colors.base },
-            BlinkCmpMenuSelection = { bg = colors.surface0 },
-            BlinkCmpLabel = { fg = colors.text },
-            BlinkCmpLabelDeprecated = { fg = colors.overlay0, style = { 'strikethrough' } },
-            -- BlinkCmpDocBorder = { fg = colors.overlay0, bg = colors.base },
-            BlinkCmpDocBorder = { fg = colors.blue, bg = colors.base },
-            BlinkCmpDoc = { fg = colors.text, bg = colors.base },
-            BlinkCmpSignatureHelpActiveParameter = { fg = colors.mauve },
-            BlinkCmpSignatureHelpBorder = { fg = colors.blue },
-
-            SnacksIndent1 = { fg = colors.red },
-            SnacksIndent2 = { fg = colors.yellow },
-            SnacksIndent3 = { fg = colors.blue },
-            SnacksIndent4 = { fg = colors.peach },
-            SnacksIndent5 = { fg = colors.green },
-            SnacksIndent6 = { fg = colors.mauve },
-            SnacksIndent7 = { fg = colors.teal },
-            SnacksIndent8 = { fg = colors.maroon },
-        }
-    end,
-    integrations = {
-        blink_cmp = true,
-        cmp = true,
-        gitsigns = true,
-        rainbow_delimiters = true,
-        treesitter_context = true,
-        indent_blankline = {
-            enabled = true,
-            scope_color = 'lavender',
-            colored_indent_levels = true,
-        },
-        flash = true,
-        mini = {
-            enabled = true,
-            indentscope_color = 'lavender',
-        },
-        telescope = { enabled = true },
-        snacks = { enabled = true, indent_scope_color = 'lavender' },
-        treesitter = true,
-        which_key = true,
-    },
-})
-vim.cmd.colorscheme('catppuccin')
-
+require('boring.colorscheme')
 require('boring.lsp')
 require('boring.plugins')

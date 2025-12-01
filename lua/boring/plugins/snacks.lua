826055789d88f9ffca3ad73d0669fb85bdacc022
Kai Mies <15252288+boringparticular@users.noreply.github.com>
2025-12-01 16:00:45 +0100

    feat(snacks): add debug stuff

--- lua/boring/plugins/snacks.lua
+++ lua/boring/plugins/snacks.lua
@@ -4,6 +4,20 @@ return {
         for_cat = 'general',
         priority = 1000,
         lazy = false,
+        beforeAll = function(_)
+            vim.api.nvim_create_autocmd('User', {
+                callback = function()
+                    -- Setup some globals for debugging (lazy-loaded)
+                    _G.dd = function(...)
+                        Snacks.debug.inspect(...)
+                    end
+                    _G.bt = function()
+                        Snacks.debug.backtrace()
+                    end
+                    vim.print = _G.dd -- Override print to use snacks for `:=` command
+                end,
+            })
+        end,
         after = function(_)
             require('snacks').setup({
                 animate = { enabled = true },
